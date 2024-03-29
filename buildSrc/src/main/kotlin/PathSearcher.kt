ea9cedfc106c45d4c393e9d29cf45238366cd530
Aaron Digulla <digulla@hepe.com>
2024-03-29 13:08:33 +0100

    Formatted

--- buildSrc/src/main/kotlin/PathSearcher.kt
+++ buildSrc/src/main/kotlin/PathSearcher.kt
@@ -1,4 +1,3 @@
-
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
@@ -6,97 +5,98 @@ import java.nio.file.Paths
 import java.util.Optional
 
 open class PathSearcher(searchPaths: List<Path>) {
-	val paths: List<Path>
-	
-	companion object {
-		fun splitPath(): List<Path> {
-			val path = System.getenv("PATH");
-			if (path.isEmpty()) {
-				throw IllegalArgumentException("The PATH environment variable is empty");
-			}
+    val paths: List<Path>
 
-			return splitPath(path);
-		}
+    companion object {
+        fun splitPath(): List<Path> {
+            val path = System.getenv("PATH");
+            if (path.isEmpty()) {
+                throw IllegalArgumentException("The PATH environment variable is empty");
+            }
 
-		fun splitPath(path: String): List<Path> {
-			if (path.isEmpty()) {
-				throw IllegalArgumentException("Search path is empty");
-			}
+            return splitPath(path);
+        }
 
-			return path.split(File.pathSeparator).map(Paths::get).toList()
-		}
-		
-		fun of(vararg paths: String) = PathSearcher(paths.map(Paths::get).toList())
-	}
-	
-    constructor(): this(splitPath())
+        fun splitPath(path: String): List<Path> {
+            if (path.isEmpty()) {
+                throw IllegalArgumentException("Search path is empty");
+            }
 
-    constructor(path: String): this(splitPath(path))
-	
-	val locator: Locator
-	init {
-		if (searchPaths.isEmpty()) {
-			throw IllegalStateException("Search path is empty");
-		}
-		
-		paths = searchPaths.map(Path::toAbsolutePath)
-		
-		locator = if (isWindows()) {
-			WindowsLocator()
-		} else {
-			UnixLocator()
-		}
-	}
-	
-	open fun isWindows(): Boolean {
-		val osName = System.getProperty("os.name")
-		return if(osName == null) {
-			false
-		} else {
-			osName.startsWith("Windows")
-		}
-	}
-	
+            return path.split(File.pathSeparator).map(Paths::get).toList()
+        }
+
+        fun of(vararg paths: String) = PathSearcher(paths.map(Paths::get).toList())
+    }
+
+    constructor() : this(splitPath())
+
+    constructor(path: String) : this(splitPath(path))
+
+    val locator: Locator
+
+    init {
+        if (searchPaths.isEmpty()) {
+            throw IllegalStateException("Search path is empty");
+        }
+
+        paths = searchPaths.map(Path::toAbsolutePath)
+
+        locator = if (isWindows()) {
+            WindowsLocator()
+        } else {
+            UnixLocator()
+        }
+    }
+
+    open fun isWindows(): Boolean {
+        val osName = System.getProperty("os.name")
+        return if (osName == null) {
+            false
+        } else {
+            osName.startsWith("Windows")
+        }
+    }
+
     open fun locate(name: String): Path {
-		for (path in paths) {
-			val located = locator.locate(name, path)
-			if (located.isPresent()) {
-				return located.get()
-			}
-		}
-		
-		throw SearchFailedException(name, paths);
-	}
+        for (path in paths) {
+            val located = locator.locate(name, path)
+            if (located.isPresent()) {
+                return located.get()
+            }
+        }
+
+        throw SearchFailedException(name, paths);
+    }
 }
 
 interface Locator {
-	fun locate(name: String, path: Path): Optional<Path>
+    fun locate(name: String, path: Path): Optional<Path>
 }
 
-class UnixLocator: Locator {
-	override fun locate(name: String, path: Path): Optional<Path> {
-		val result = path.resolve(name)
-		if (Files.exists(result)) {
-			return Optional.of(result)
-		}
-		
-		return Optional.empty()
-	}
+class UnixLocator : Locator {
+    override fun locate(name: String, path: Path): Optional<Path> {
+        val result = path.resolve(name)
+        if (Files.exists(result)) {
+            return Optional.of(result)
+        }
+
+        return Optional.empty()
+    }
 }
 
-class WindowsLocator: Locator {
-	override fun locate(name: String, path: Path): Optional<Path> {
-		val ext = (System.getenv("PATHEXT") ?: ".COM;.EXE;.BAT;.CMD").split(";")
-		for (it in ext) {
-			val result = path.resolve(name + ext)
-			if (Files.exists(result)) {
-				return Optional.of(result)
-			}
-		}
+class WindowsLocator : Locator {
+    override fun locate(name: String, path: Path): Optional<Path> {
+        val ext = (System.getenv("PATHEXT") ?: ".COM;.EXE;.BAT;.CMD").split(";")
+        for (it in ext) {
+            val result = path.resolve(name + ext)
+            if (Files.exists(result)) {
+                return Optional.of(result)
+            }
+        }
 
-		return Optional.empty()
-	}
+        return Optional.empty()
+    }
 }
 
-class SearchFailedException(name: String, paths: List<Path>):
-	RuntimeException("Unable to locate '${name}' in:\n${paths.joinToString(separator = "\n", prefix = "- '", postfix = "'")}") {}
+class SearchFailedException(name: String, paths: List<Path>) :
+        RuntimeException("Unable to locate '${name}' in:\n${paths.joinToString(separator = "\n", prefix = "- '", postfix = "'")}") {}
--- buildSrc/src/main/kotlin/PathSearcherBuilder.kt
+++ buildSrc/src/main/kotlin/PathSearcherBuilder.kt
@@ -1,44 +1,43 @@
-
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import org.slf4j.LoggerFactory
 
 open class PathSearcherBuilder(val paths: MutableList<Path> = mutableListOf()) {
-	private val log = LoggerFactory.getLogger(javaClass)
-		
-	fun addFromEnvionment(name: String, vararg extension: String): PathSearcherBuilder {
-		val value = System.getenv(name)
-		if (value == null) {
-			throw IllegalStateException("Environment variable ${name} is not defined")
-		}
-		
-		var path = Paths.get(value)
-		for(it in extension) {
-			path = path.resolve(it)
-		}
-		
-		return add(path)
-	}
-	
-	fun add(path: String): PathSearcherBuilder {
-		return add(Paths.get(path))
-	}
-	
-	fun add(path: Path): PathSearcherBuilder {
-		val absPath = path.toAbsolutePath()
-		if (!Files.exists(path)) {
-			log.warn("Adding non-existing folder to path: {}", absPath)
-		} else if (!Files.isDirectory(path)) {
-			log.warn("Adding non-folder to path: {}", absPath)
-		}
-		
-		paths.add(path)
+    private val log = LoggerFactory.getLogger(javaClass)
 
-		return this
-	}
-	
-	fun build(): PathSearcher {
-		return PathSearcher(paths)
-	}
+    fun addFromEnvionment(name: String, vararg extension: String): PathSearcherBuilder {
+        val value = System.getenv(name)
+        if (value == null) {
+            throw IllegalStateException("Environment variable ${name} is not defined")
+        }
+
+        var path = Paths.get(value)
+        for (it in extension) {
+            path = path.resolve(it)
+        }
+
+        return add(path)
+    }
+
+    fun add(path: String): PathSearcherBuilder {
+        return add(Paths.get(path))
+    }
+
+    fun add(path: Path): PathSearcherBuilder {
+        val absPath = path.toAbsolutePath()
+        if (!Files.exists(path)) {
+            log.warn("Adding non-existing folder to path: {}", absPath)
+        } else if (!Files.isDirectory(path)) {
+            log.warn("Adding non-folder to path: {}", absPath)
+        }
+
+        paths.add(path)
+
+        return this
+    }
+
+    fun build(): PathSearcher {
+        return PathSearcher(paths)
+    }
 }
